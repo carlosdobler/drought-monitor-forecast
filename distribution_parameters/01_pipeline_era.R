a8b4a869bd5ecd36b8d793e458335827f4cc5675
cdobler-risk-vm-r-01 <dobler.carlos@gmail.com>
2026-01-07 19:30:01 +0000

    tiling approach

--- distribution_parameters/01_pipeline_era.R
+++ distribution_parameters/01_pipeline_era.R
@@ -41,126 +41,311 @@ mask <-
 date_vector_full <-
   seq(as_date("1990-01-01"), as_date("2020-12-01"), by = "1 month")
 
-
-# ******
-
-## LOAD TAS AND PRECIP ----
-
-# temperature
-s_tas <-
-  load_data(
-    dir_origin_cloud = "gs://clim_data_reg_useast1/era5/monthly_means/2m_temperature/",
-    dir_dest_local = dir_data,
-    date_vector = date_vector_full
-  )
-
-# mask land
-s_tas[is.na(mask)] <- NA
-
-
-# precipitation
-s_precip <-
-  load_data(
-    dir_origin_cloud = "gs://clim_data_reg_useast1/era5/monthly_means/total_precipitation/",
-    dir_dest_local = dir_data,
-    date_vector = date_vector_full
-  )
-
-# mask land
-s_precip[is.na(mask)] <- NA
-
-# convert to mm/month
-
 conv <-
   (units::set_units(1, month) / units::set_units(1, d)) |>
   units::drop_units()
 
-s_precip <-
-  s_precip |>
-  units::drop_units() |>
-  mutate(
-    tp = tp * 1000 * conv,
-    tp = units::set_units(tp, mm / month)
-  )
 
-## FIT DISTR TAS AND PRECIP ----
-
-# temperature
-distr_params(
-  s_tas |> filter(year(time) >= 1991),
-  dir_tmp_local = fs::path(dir_data, "tmp"),
-  dir_output_cloud = "gs://clim_data_reg_useast1/era5/climatologies/",
-  distribution = pelgno,
-  f_name_root = "era5_2m-temperature_mon_norm-params_1991-2020",
-  process = "era"
-)
+df_tiles <-
+  rt_tile_table(mask, 300, mask)
 
 
-# precipitation
-distr_params(
-  s_precip |> filter(year(time) >= 1991),
-  dir_tmp_local = fs::path(dir_data, "tmp"),
-  dir_output_cloud = "gs://clim_data_reg_useast1/era5/climatologies_monthly_totals/",
-  distribution = lmom::pelgam,
-  f_name_root = "era5_total-precipitation_mon_gamma-params_1991-2020",
-  process = "era"
-)
+# ******
+
+# DOWNLOAD TAS AND PRECIP DATA
+
+fff <-
+  c(
+    tas = "gs://clim_data_reg_useast1/era5/monthly_means/2m_temperature/",
+    precip = "gs://clim_data_reg_useast1/era5/monthly_means/total_precipitation/"
+  ) |>
+  map(\(dir) {
+    rt_gs_list_files(dir) |>
+      str_subset(
+        str_flatten(seq(1990, 2020), "|")
+      )
+  })
+
+fff <-
+  fff |>
+  map(\(ff) {
+    ff |>
+      rt_gs_download_files(dir_data, quiet = T)
+  })
+
+# some files were saved with time dimension (1 timestep)
+# this messes up following steps
+# identify which ones have 2 vs 3 dims
+
+dims_length <-
+  fff |>
+  map(\(ff) {
+    ff |>
+      map_int(\(f) {
+        f |>
+          read_mdim(proxy = T) |>
+          dim() |>
+          length()
+      })
+  })
 
 
-## CALCULATE WATER BALANCE ----
+# BY TILES
 
-# calculate PET
-s_pet <-
-  date_vector_full |>
-  map(\(d) {
-    pet_calculator_hamon(
-      d,
-      s_tas |>
-        filter(time == d) |>
-        adrop()
+df_tiles_m <-
+  df_tiles |>
+  filter(mask == T)
+
+for (i in seq(nrow(df_tiles_m))) {
+  #
+  message(" ")
+  message(str_glue("processing tile {i} / {nrow(df_tiles_m)}"))
+
+  # LOAD DATA
+
+  df_matrix <-
+    cbind(
+      c((df_tiles_m$start_x[i] - 1), (df_tiles_m$start_y[i] - 1), 0),
+      c(df_tiles_m$count_x[i], df_tiles_m$count_y[i], NA)
     )
-  })
 
-s_pet <-
-  do.call(c, c(s_pet, along = "time")) |>
-  st_set_dimensions("time", values = date_vector_full)
+  ss <-
+    map2(fff, dims_length, \(ff, dim_length) {
+      #
+      s_tile <-
+        future_map2(ff, dim_length, \(f, d) {
+          #
+          if (d == 2) {
+            df_matrix <- df_matrix[1:2, ]
+          }
 
-# calculate WB
-s_pet <-
-  s_pet |>
-  units::drop_units() |>
-  mutate(
-    pet = pet * conv, # convert to mm/month
-    pet = units::set_units(pet, mm / month)
+          read_mdim(
+            f,
+            offset = df_matrix[, 1],
+            count = df_matrix[, 2]
+          ) |>
+            adrop()
+        })
+
+      s_tile <-
+        do.call(c, c(s_tile, along = "time")) |>
+        st_set_dimensions("time", values = date_vector_full)
+
+      return(s_tile)
+    })
+
+  # MASK LAND
+  mask_sub <-
+    mask[,
+      df_tiles_m$start_x[i]:df_tiles_m$end_x[i],
+      df_tiles_m$start_y[i]:df_tiles_m$end_y[i]
+    ]
+
+  ss <-
+    ss |>
+    map(\(s) {
+      s[is.na(mask_sub)] <- NA
+      return(s)
+    })
+
+  # convert precip to mm/month
+  ss[[2]] <-
+    ss[[2]] |>
+    units::drop_units() |>
+    mutate(
+      tp = tp * 1000 * conv,
+      tp = units::set_units(tp, mm / month)
+    )
+
+  # FIT DISTRIBUTIONS
+
+  pwalk(
+    list(ss, list(pelgno, pelgam), names(ss)),
+    \(s, distribution, variable) {
+      #
+      message(str_glue("fitting {variable}"))
+
+      # get names from simulated l-moments vector
+      param_names <-
+        names(distribution(c(1, 0.1, 0.1, 0.1)))
+
+      # split s by calendar months
+      s_mon <-
+        map(set_names(seq(12)), \(mon) {
+          s |>
+            filter(year(time) >= 1991, month(time) == mon) |>
+            units::drop_units()
+        })
+
+      future_iwalk(s_mon, \(s, mon) {
+        #
+        r <-
+          s |>
+          st_apply(
+            c(1, 2),
+            distr_params_apply,
+            param_names = param_names,
+            distribution = distribution,
+            .fname = "params"
+          ) |>
+          split("params")
+        #
+
+        rt_write_nc(
+          r,
+          str_glue(
+            "{dir_data}/distr_tile-{df_tiles_m$tile_id[i]}_mon-{str_pad(mon, 2, 'left', '0')}_var-{v}.nc",
+            v = variable
+          )
+        )
+      })
+    }
   )
 
-s_wb <-
-  c(s_precip, s_pet) |>
-  mutate(wb = tp - pet) |>
-  select(wb)
+  # CALCULATE PET
+  s_pet <-
+    date_vector_full |>
+    map(\(d) {
+      pet_calculator_hamon(
+        d,
+        ss[[1]] |>
+          filter(time == d) |>
+          adrop()
+      )
+    })
 
-## INTEGRATION WINDOWS ----
+  s_pet <-
+    do.call(c, c(s_pet, along = "time")) |>
+    st_set_dimensions("time", values = date_vector_full)
 
-ss_wb_rolled <-
-  c(3, 12) |>
-  set_names() |>
-  map(\(k_) {
-    print(k_)
-    rollsum(s_wb, k_, "wb_rollsum")
+  # convert to mm/month
+  s_pet <-
+    s_pet |>
+    units::drop_units() |>
+    mutate(
+      pet = pet * conv, # convert to mm/month
+      pet = units::set_units(pet, mm / month)
+    )
+
+  # CALCULATE WB
+  s_wb <-
+    c(ss[[2]], s_pet) |>
+    mutate(wb = tp - pet) |>
+    select(wb)
+
+  # INTEGRATION WINDOWS
+  message(str_glue("roll-summing"))
+
+  ss_wb_rolled <-
+    c(3, 12) |>
+    set_names() |>
+    map(\(k_) {
+      rollsum(s_wb, k_, "wb_rollsum")
+    })
+
+  # FIT DISTRIBUTION
+  iwalk(ss_wb_rolled, \(s, k_) {
+    #
+    message(str_glue("fitting wb window {k_}"))
+
+    # get names from simulated l-moments vector
+    param_names <-
+      names(pelglo(c(1, 0.1, 0.1, 0.1)))
+
+    # split s by calendar months
+    s_mon <-
+      map(set_names(seq(12)), \(mon) {
+        s |>
+          filter(year(time) >= 1991, month(time) == mon) |>
+          units::drop_units()
+      })
+
+    future_iwalk(s_mon, \(s, mon) {
+      #
+      r <-
+        s |>
+        st_apply(
+          c(1, 2),
+          distr_params_apply,
+          param_names = param_names,
+          distribution = pelglo,
+          .fname = "params"
+        ) |>
+        split("params")
+      #
+
+      rt_write_nc(
+        r,
+        str_glue(
+          "{dir_data}/distr_tile-{df_tiles_m$tile_id[i]}_mon-{str_pad(mon, 2, 'left', '0')}_var-wb-{k_}.nc",
+          k_ = k_
+        )
+      )
+    })
+  })
+}
+
+# MOSAIC ALL
+
+list(
+  c(
+    "tas",
+    "precip",
+    "wb-3",
+    "wb-12"
+  ),
+  c(
+    "2m-temperature",
+    "total-precipitation",
+    "water-balance-hamon-rollsum3",
+    "water-balance-hamon-rollsum12"
+  ),
+  c(
+    "norm",
+    "gamma",
+    "log",
+    "log"
+  ),
+  c(
+    "climatologies",
+    "climatologies_monthly_totals",
+    "climatologies_monthly_totals",
+    "climatologies_monthly_totals"
+  )
+) |>
+  pwalk(\(variable, long_var, dist, dir) {
+    #
+    message(str_glue("mosaicking {variable}"))
+
+    ff <-
+      dir_data |>
+      fs::dir_ls(regexp = str_glue("_var-{variable}"))
+
+    walk(str_pad(seq(12), 2, "left", "0"), \(mon) {
+      #
+      message(str_glue("   {mon} / 12"))
+
+      ss_tiles <-
+        ff |>
+        str_subset(str_glue("mon-{mon}")) |>
+        map(read_mdim)
+
+      mos <-
+        rt_mosaic(ss_tiles)
+
+      f_res <- str_glue("{dir_data}/era5_{long_var}_mon_{dist}-params_1991-2020_{mon}.nc")
+
+      rt_write_nc(
+        mos,
+        f_res
+      )
+
+      str_glue("gcloud storage mv {f_res} gs://clim_data_reg_useast1/era5/{dir}") |>
+        system(ignore.stdout = T, ignore.stderr = T)
+    })
+
+    fs::file_delete(ff)
   })
 
-## FIT DISTR WB ----
-
-iwalk(ss_wb_rolled, \(s, k) {
-  print(k)
-
-  distr_params(
-    s |> filter(year(time) >= 1991),
-    dir_tmp_local = fs::path(dir_data, "tmp"),
-    dir_output_cloud = "gs://clim_data_reg_useast1/era5/climatologies_monthly_totals/",
-    distribution = pelglo,
-    f_name_root = str_glue("era5_water-balance-hamon-rollsum{k}_mon_log-params_1991-2020"),
-    process = "era",
-    future_ = F
-  )
-})
+fff |>
+  walk(fs::file_delete)

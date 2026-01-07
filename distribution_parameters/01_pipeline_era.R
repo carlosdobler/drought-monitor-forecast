dfc76dd78f07e99be41ca83e266321a419ce943e
cdobler-risk-vm-r-01 <dobler.carlos@gmail.com>
2026-01-07 03:14:09 +0000

    m/d to mm/month; hamon pet formulation

--- distribution_parameters/01_pipeline_era.R
+++ distribution_parameters/01_pipeline_era.R
@@ -6,16 +6,17 @@
 
 library(tidyverse)
 library(stars)
-library(mirai)
+library(furrr)
 library(lmom)
 
-daemons(parallel::detectCores() - 1)
+plan(multicore, workers = parallelly::availableCores() - 1)
 
 # set data directory
 source("distribution_parameters/setup.R")
 
 # load general functions
 source("functions/general_tools.R")
+source("functions/tile.R")
 
 # load general drought functions
 source("functions/functions_drought.R")
@@ -36,6 +37,7 @@ mask <-
 
 
 # dates to process
+# add 1 year at the beginning for rolling windows
 date_vector_full <-
   seq(as_date("1990-01-01"), as_date("2020-12-01"), by = "1 month")
 
@@ -53,7 +55,7 @@ s_tas <-
   )
 
 # mask land
-s_tas[mask == 0] <- NA
+s_tas[is.na(mask)] <- NA
 
 
 # precipitation
@@ -65,14 +67,20 @@ s_precip <-
   )
 
 # mask land
-s_precip[mask == 0] <- NA
+s_precip[is.na(mask)] <- NA
 
 # convert to mm/month
+
+conv <-
+  (units::set_units(1, month) / units::set_units(1, d)) |>
+  units::drop_units()
+
 s_precip <-
   s_precip |>
+  units::drop_units() |>
   mutate(
-    tp = tp / units::set_units(1, d), # m -> mm -> x 30 days
-    tp = units::set_units(tp, mm / month) |> round()
+    tp = tp * 1000 * conv,
+    tp = units::set_units(tp, mm / month)
   )
 
 ## FIT DISTR TAS AND PRECIP ----
@@ -80,7 +88,7 @@ s_precip <-
 # temperature
 distr_params(
   s_tas |> filter(year(time) >= 1991),
-  dir_tmp_local = dir_data,
+  dir_tmp_local = fs::path(dir_data, "tmp"),
   dir_output_cloud = "gs://clim_data_reg_useast1/era5/climatologies/",
   distribution = pelgno,
   f_name_root = "era5_2m-temperature_mon_norm-params_1991-2020",
@@ -91,71 +99,68 @@ distr_params(
 # precipitation
 distr_params(
   s_precip |> filter(year(time) >= 1991),
-  dir_tmp_local = dir_data,
+  dir_tmp_local = fs::path(dir_data, "tmp"),
   dir_output_cloud = "gs://clim_data_reg_useast1/era5/climatologies_monthly_totals/",
   distribution = lmom::pelgam,
   f_name_root = "era5_total-precipitation_mon_gamma-params_1991-2020",
   process = "era"
 )
 
-# THIS SECTION NEEDS TO BE RECALCULATED WITH MONTHLY TOTAL PRECIP
 
-# ## CALCULATE WATER BALANCE ----
+## CALCULATE WATER BALANCE ----
 
-# # heat index constants
+# calculate PET
+s_pet <-
+  date_vector_full |>
+  map(\(d) {
+    pet_calculator_hamon(
+      d,
+      s_tas |>
+        filter(time == d) |>
+        adrop()
+    )
+  })
 
-# heat_vars <- heat_index_var_generator()
+s_pet <-
+  do.call(c, c(s_pet, along = "time")) |>
+  st_set_dimensions("time", values = date_vector_full)
 
-# # calculate water balance
+# calculate WB
+s_pet <-
+  s_pet |>
+  units::drop_units() |>
+  mutate(
+    pet = pet * conv, # convert to mm/month
+    pet = units::set_units(pet, mm / month)
+  )
 
-# s_wb <-
-#   date_vector_full |>
-#   map(\(d) {
-#     wb_calculator_th(
-#       #
-#       d,
+s_wb <-
+  c(s_precip, s_pet) |>
+  mutate(wb = tp - pet) |>
+  select(wb)
 
-#       s_tas |>
-#         filter(time == d) |>
-#         adrop() |>
-#         setNames("tas") |>
-#         mutate(tas = tas |> units::set_units(degC)),
+## INTEGRATION WINDOWS ----
 
-#       s_precip |>
-#         filter(time == d) |>
-#         adrop() |>
-#         setNames("pr"),
+ss_wb_rolled <-
+  c(3, 12) |>
+  set_names() |>
+  map(\(k_) {
+    print(k_)
+    rollsum(s_wb, k_, "wb_rollsum")
+  })
 
-#       heat_vars
-#     )
-#   })
+## FIT DISTR WB ----
 
-# s_wb <-
-#   do.call(c, c(s_wb, along = "time")) |>
-#   st_set_dimensions("time", values = date_vector_full)
+iwalk(ss_wb_rolled, \(s, k) {
+  print(k)
 
-# ## INTEGRATION WINDOWS ----
-
-# ss_wb_rolled <-
-#   c(3, 12) |>
-#   set_names() |>
-#   map(\(k_) {
-#     print(k_)
-
-#     rollsum(s_wb, k_, "wb_rollsum")
-#   })
-
-# ## FIT DISTR WB ----
-
-# iwalk(ss_wb_rolled, \(s, k) {
-#   print(k_)
-
-#   distr_params(
-#     s |> filter(year(time) >= 1991),
-#     dir_tmp_local = "/mnt/pers_disk/tmp",
-#     dir_output_cloud = "gs://clim_data_reg_useast1/era5/climatologies/",
-#     distribution = pelglo,
-#     f_name_root = str_glue("era5_water-balance-th-rollsum{k}_mon_log-params_1991-2020"),
-#     process = "era"
-#   )
-# })
+  distr_params(
+    s |> filter(year(time) >= 1991),
+    dir_tmp_local = fs::path(dir_data, "tmp"),
+    dir_output_cloud = "gs://clim_data_reg_useast1/era5/climatologies_monthly_totals/",
+    distribution = pelglo,
+    f_name_root = str_glue("era5_water-balance-hamon-rollsum{k}_mon_log-params_1991-2020"),
+    process = "era",
+    future_ = F
+  )
+})

3af20469567815f23dcdd4b5a34b17976fd0673f
cdobler-risk-vm-r-01 <dobler.carlos@gmail.com>
2026-01-09 03:14:53 +0000

    hamon pet; efficieny changes; m to mm

--- monitor_forecast/1_era_monitor_generator.R
+++ monitor_forecast/1_era_monitor_generator.R
@@ -2,132 +2,96 @@
 # BASED ON A MODIFIED SPEI METHODOLOGY
 # USES THORNWHAITES FORMULATION TO CALCULATE PET
 
-# loop through integration windows
-for (k in winds) {
-  message(str_glue("*** PROCESSING {date_to_proc} (window = {k}) ***"))
+# extract what wb quantiles dates have already been processed
+existing_dates_final_bucket <-
+  rt_gs_list_files(str_glue("gs://drought-monitor/historical")) |>
+  str_subset(".nc$") |>
+  str_subset(str_glue("-w12_")) |>
+  str_sub(-13, -4)
 
-  # extract what wb quantiles dates have already been processed
-  # existing_dates <-
-  #   rt_gs_list_files(str_glue("gs://drought-monitor/historical")) |>
-  #   str_subset(".nc$") |>
-  #   str_subset(str_glue("-w{k}_")) |>
-  #   str_sub(-13, -4)
+if (date_to_proc %in% existing_dates_final_bucket) {
+  stop("THIS DATE HAS ALREADY BEEN PROCESSED!")
+}
 
-  # if (date_to_proc %in% existing_dates) {
-  #   # loop to next integration window if date already exists
+# vector of all dates necessary to calculate rolling sum
+pre_dates <-
+  seq(
+    as_date(date_to_proc) - months(12 - 1), # max integration window
+    as_date(date_to_proc),
+    by = "1 month"
+  )
 
-  #   message("THIS DATE HAS ALREADY BEEN PROCESSED! Skipping process")
-  #   next
-  #   #
-  # } else {
-  # start process if date does not exist
 
-  # vector of all dates necessary to calculate rolling sum
-  pre_dates_n <-
-    seq(
-      as_date(date_to_proc) - months(k - 1),
-      as_date(date_to_proc),
-      by = "1 month"
+# CHECK AVAILABLE FILES
+
+# existing wb files in bucket
+existing_dates_bucket <-
+  rt_gs_list_files(str_glue(
+    "{dir_gs_era}/monthly_totals/water_balance_hamon/*.nc"
+  )) |>
+  suppressWarnings() |>
+  str_sub(-13, -4)
+
+dates_in_bucket <-
+  pre_dates[pre_dates %in% existing_dates_bucket]
+
+# ... as inputs to calculate wb (temperature and precip)
+existing_dates_inputs <-
+  c("total_precipitation", "2m_temperature") |>
+  set_names() |>
+  map(\(var) {
+    rt_gs_list_files(str_glue("{dir_gs_era}/monthly_means/{var}")) |>
+      str_sub(-13, -4)
+  })
+
+dates_inputs <-
+  pre_dates[pre_dates %in% existing_dates_inputs[[1]]]
+
+
+# DOWNLOAD SECTION
+
+# if some wb dates are in the bucket, download
+if (length(dates_in_bucket) > 0) {
+  ff <-
+    str_glue(
+      "{dir_gs_era}/monthly_totals/water_balance_hamon/era5_water-balance-hamon_mon_{dates_in_bucket}.nc"
     )
 
-  # if this is not the first window, check what wb dates are in dir_data
-  if (k == winds[1]) {
-    pre_dates <- pre_dates_n
-  } else {
-    existing_dates_wb_in_disk <-
-      fs::dir_ls(dir_data) |>
-      str_subset("era5_water-balance-th_mon_") |>
-      str_sub(-13, -4)
+  rt_gs_download_files(ff, dir_data, quiet = T) |>
+    invisible()
+}
 
-    # update pre_dates:
-    # only dates not in disk
-    pre_dates <-
-      pre_dates_n[!pre_dates_n %in% existing_dates_wb_in_disk]
-  }
-
-  # check if necessary wb dates are available in bucket
-  existing_dates_wb <-
-    rt_gs_list_files(str_glue(
-      "{dir_gs_era}/monthly_means/water_balance_th/*.nc"
-    )) |>
-    str_sub(-13, -4)
-
-  # existing_dates_wb <- existing_dates_wb |> head(-2)
-
-  if (any(pre_dates %in% existing_dates_wb)) {
-    # if any wb date is available, download it from bucket
-
-    existing_pre_dates_wb <-
-      pre_dates[pre_dates %in% existing_dates_wb]
-
-    existing_pre_dates_wb |>
-      walk(\(d) {
-        message(str_glue("   Downloading wb {d} from bucket"))
-
-        f <- str_glue("era5_water-balance-th_mon_{d}.nc")
-
-        rt_gs_download_files(
-          str_glue("{dir_gs_era}/monthly_means/water_balance_th/{f}"),
-          dir_data,
-          quiet = T
-        )
-      })
-  }
-
-  # This section runs if not all wb pre-dates are available in bucket or disk
-  if (any(!pre_dates %in% existing_dates_wb)) {
-    missing_pre_dates_wb <-
-      pre_dates[!pre_dates %in% existing_dates_wb]
-
-    # identify missing wb dates that are available as tas and precip (wb inputs)
-    existing_pre_dates_inputs <-
-      c("total_precipitation", "2m_temperature") |>
-      set_names() |>
-      map(\(var) {
-        existing_dates_1_input <-
-          rt_gs_list_files(str_glue("{dir_gs_era}/monthly_means/{var}")) |>
-          str_sub(-13, -4)
-
-        # existing_dates_1_input <- existing_dates_1_input |> head(-1)
-
-        # missing wb: also missing pr and/or tas?
-        # (boolean vector of length: missing wb months == TRUE)
-        missing_pre_dates_wb[missing_pre_dates_wb %in% existing_dates_1_input]
-      })
-
-    if (length(unlist(existing_pre_dates_inputs)) > 0) {
-      # if there are dates available as inputs, download them
-
-      existing_pre_dates_inputs |>
-        iwalk(\(d, var) {
-          message(str_glue("   Downloading {var} {d} from bucket"))
-
-          f <- str_glue("era5_{str_replace_all(var, '_', '-')}_mon_{d}.nc")
-
-          rt_gs_download_files(
-            str_glue("{dir_gs_era}/monthly_means/{var}/{f}"),
-            dir_data,
-            quiet = T
+# if there are wb dates that are not in the bucket,
+# but are as inputs, download
+if (any(!dates_inputs %in% dates_in_bucket)) {
+  ff <-
+    c("total_precipitation", "2m_temperature") |>
+    map(\(var) {
+      rt_gs_list_files(str_glue("{dir_gs_era}/monthly_means/{var}")) |>
+        str_subset(
+          str_flatten(
+            str_glue("_{dates_inputs[!dates_inputs %in% dates_in_bucket]}.nc"),
+            "|"
           )
-        })
-    }
+        )
+    })
 
-    # This section runs if there are missing dates as inputs in buckets
-    # Downloads them from cds
-    if (length(unlist(existing_pre_dates_inputs)) < length(missing_pre_dates_wb) * 2) {
-      # k*2 = 2 inputs (pr and tas) x the number of missing (needed) dates
+  ff |>
+    walk(rt_gs_download_files, dir_data, quiet = T)
+}
 
-      missing_pre_dates_inputs <-
-        existing_pre_dates_inputs |>
-        map(\(var) {
-          missing_pre_dates_wb[!missing_pre_dates_wb %in% var]
-        })
+# if there are dates that are not as inputs, download from cds
+if (any(!pre_dates %in% dates_inputs)) {
+  #
+  missing_dates <- pre_dates[!pre_dates %in% dates_inputs]
 
-      reticulate::py_require("cdsapi")
-      cdsapi <- reticulate::import("cdsapi")
+  reticulate::py_require("cdsapi")
+  cdsapi <- reticulate::import("cdsapi")
 
-      missing_pre_dates_inputs |>
-        iwalk(\(d, var) {
+  missing_dates |>
+    walk(\(d) {
+      c("total_precipitation", "2m_temperature") |>
+        walk(\(var) {
           message(str_glue("   Downloading {var} {d} from cds"))
 
           f <- str_glue("era5_{str_replace_all(var, '_', '-')}_mon_{d}.nc")
@@ -164,102 +128,134 @@ for (k in winds) {
           str_glue("gcloud storage cp {dir_data}/{f} {dir_gs_era}/monthly_means/{var}/") |>
             system(ignore.stdout = T, ignore.stderr = T)
         })
-    }
-
-    # calculate water balance for missing dates
-
-    # heat index constants
-    heat_vars <- heat_index_var_generator()
-
-    # wb
-    missing_pre_dates_wb |>
-      walk(\(d) {
-        # for each month
-
-        # load temperature
-        s_tas <-
-          read_ncdf(str_glue(
-            "{dir_data}/era5_2m-temperature_mon_{as_date(d)}.nc"
-          )) |>
-          suppressMessages() |>
-          adrop()
-
-        # load precipitation
-        s_pr <-
-          read_ncdf(str_glue(
-            "{dir_data}/era5_total-precipitation_mon_{as_date(d)}.nc"
-          )) |>
-          suppressMessages() |>
-          adrop()
-
-        # calculate wb
-        s_wb <-
-          wb_calculator_th(
-            d,
-
-            s_tas |>
-              setNames("tas") |>
-              mutate(tas = tas |> units::set_units(degC)),
-
-            s_pr |>
-              setNames("pr"),
-
-            heat_vars
-          )
-
-        # save to disk
-        f_res <- str_glue("{dir_data}/era5_water-balance-th_mon_{d}.nc")
-        rt_write_nc(s_wb, f_res)
-
-        # upload to bucket
-        str_glue(
-          "gcloud storage cp {f_res} {dir_gs_era}/monthly_means/water_balance_th/"
-        ) |>
-          system(ignore.stdout = T, ignore.stderr = T)
-      })
-  }
-
-  #
-  #
-  #
-
-  # load all wb files
-  s_wb <-
-    pre_dates_n |>
-    map(\(d) {
-      str_glue("{dir_data}/era5_water-balance-th_mon_{d}.nc") |>
-        read_ncdf() |>
-        suppressMessages()
     })
+} else {
+  missing_dates <- NULL
+}
 
-  # concatenate all months
+
+# IMPORT WB DATES IN DISK
+
+f_wb <-
+  fs::dir_ls(dir_data) |>
+  str_subset("era5_water-balance-hamon_mon_")
+
+if (length(f_wb) > 0) {
+  #
+  dates_wb <-
+    f_wb |>
+    str_sub(-13, -4)
+
+  s_wb_disk <-
+    f_wb |>
+    map(read_mdim)
+
+  s_wb_disk <-
+    do.call(c, c(s_wb_disk, along = "time")) |>
+    st_set_dimensions("time", values = as_date(dates_wb))
+}
+
+# CALCULATE MISSING WB DATES
+
+dates_missing_wb <-
+  c(dates_inputs[!dates_inputs %in% dates_in_bucket], missing_dates)
+
+ss_inputs <-
+  c("total_precipitation", "2m_temperature") |>
+  map(\(var) {
+    ss <-
+      str_glue(
+        "{dir_data}/era5_{str_replace_all(var, '_', '-')}_mon_{dates_missing_wb}.nc"
+      ) |>
+      map(read_mdim) |>
+      map(adrop)
+
+    return(ss)
+  })
+
+s_pet <-
+  map2(dates_missing_wb, ss_inputs[[2]], \(d, s) {
+    pet_calculator_hamon(d, s)
+  })
+
+s_pet <-
+  do.call(c, c(s_pet, along = "time")) |>
+  st_set_dimensions("time", values = dates_missing_wb)
+
+# convert to mm/month
+s_pet <-
+  s_pet |>
+  units::drop_units() |>
+  mutate(
+    pet = pet * conv, # convert to mm/month
+    pet = units::set_units(pet, mm / month)
+  )
+
+s_pr <-
+  do.call(c, c(ss_inputs[[1]], along = "time")) |>
+  st_set_dimensions("time", values = dates_missing_wb) |>
+  units::drop_units() |>
+  mutate(
+    tp = tp * 1000 * conv,
+    tp = units::set_units(tp, mm / month)
+  )
+
+s_wb <-
+  c(s_pr, s_pet) |>
+  mutate(wb = tp - pet) |>
+  select(wb)
+
+# save wb to bucket
+dates_missing_wb |>
+  walk(\(d) {
+    f <- str_glue("{dir_data}/era5_water-balance-hamon_mon_{d}.nc")
+
+    s_wb |>
+      filter(time == d) |>
+      adrop() |>
+      rt_write_nc(f)
+
+    str_glue("gcloud storage mv {f} {dir_gs_era}/monthly_totals/water_balance_hamon/") |>
+      system(ignore.stdout = T, ignore.stderr = T)
+  })
+
+# COMBINE WITH WB IN DISK
+
+if (length(f_wb) > 0) {
   s_wb <-
-    do.call(c, c(s_wb, along = "time"))
+    c(s_wb_disk, s_wb)
+}
+
+
+# CALCULATE PERCENTILES (QUANTILE)
+
+# loop through integration windows
+for (k in c(12, 3)) {
+  message(str_glue("*** PROCESSING {date_to_proc} (window = {k}) ***"))
+
+  pre_dates_k <- pre_dates |> tail(k)
 
   # aggregate (rollsum)
   s_wb_rolled <-
     s_wb |>
+    filter(time %in% pre_dates_k) |>
     st_apply(c(1, 2), sum, .fname = str_glue("wb_rollsum{k}"))
 
-  message(str_glue("   Calculating anomalies"))
-
   # import baseline distribution parameters
-
   f_distr <-
     str_glue(
-      "era5_water-balance-th-rollsum{k}_mon_log-params_1991-2020_{str_pad(month(as_date(date_to_proc)), 2, 'left', '0')}.nc"
+      "era5_water-balance-hamon-rollsum{k}_mon_log-params_1991-2020_{str_pad(month(as_date(date_to_proc)), 2, 'left', '0')}.nc"
     )
 
   s_dist_params <-
     rt_gs_download_files(
-      str_glue("{dir_gs_era}/climatologies/{f_distr}"),
+      str_glue("{dir_gs_era}/climatologies_monthly_totals/{f_distr}"),
       dir_data,
       quiet = T
     ) |>
-    read_ncdf() |>
-    suppressMessages()
+    read_mdim()
 
-  # calculate quantile
+  # calculate percentile (quantile)
 
   s_perc <-
     c(s_wb_rolled, s_dist_params) |>
@@ -267,17 +263,16 @@ for (k in winds) {
     st_apply(
       c(1, 2),
       \(x) {
-        if (any(is.na(x))) {
+        if (is.na(x)[2]) {
           NA
         } else {
           lmom::cdfglo(x[1], c(x[2], x[3], x[4]))
         }
       },
-      # FUTURE = T,
       .fname = "perc"
     )
 
-  # generte plot to check with spei csic
+  # # plot to check with spei csic
   # s_perc |>
   #   st_warp(st_as_stars(st_bbox(), dx = 0.25, values = NA)) |>
   #   as_tibble() |>
@@ -291,28 +286,24 @@ for (k in winds) {
   #                                           n.breaks = 11)
 
   # save result
-
   res_file <-
     str_glue(
-      "era5_water-balance-perc-w{k}_bl-1991-2020_mon_{date_to_proc}.nc"
+      "{dir_data}/era5_water-balance-perc-w{k}_bl-1991-2020_mon_{date_to_proc}.nc"
     )
-
-  res_path <-
-    str_glue("{dir_data}/{res_file}")
 
   rt_write_nc(
     s_perc,
-    res_path,
+    res_file,
     gatt_name = "source code",
     gatt_val = "https://github.com/carlosdobler/drought-monitor-forecast"
   )
 
-  # upload to gcloud
-  # str_glue("gsutil mv {res_path} {dir_gs_era}/water_balance_th_perc/") |>
-  #   system(ignore.stdout = T, ignore.stderr = T)
-  str_glue("gcloud storage mv {res_path} gs://drought-monitor/historical/") |>
+  # upload to cloud
+  str_glue("gcloud storage mv {res_file} gs://drought-monitor/historical/") |>
     system(ignore.stdout = T, ignore.stderr = T)
-  # } # end of else (if wb quantile was missing)
-} # end of window loop
+}
 
-# fs::dir_delete(dir_data)
+# clean up
+dir_data |>
+  fs::dir_ls() |>
+  fs::file_delete()

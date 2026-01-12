09db67686d1da4169db540a8e6c5c70333baedf7
cdobler-risk-vm-r-01 <dobler.carlos@gmail.com>
2026-01-12 17:29:51 +0000

    removed functions; -99999 to avoid gdal misinterpreting

--- functions/functions_distributions.R
+++ functions/functions_distributions.R
@@ -37,6 +37,8 @@ land_mask <- function(ref_grid) {
     st_warp(ref_grid, method = "max", use_gdal = T) |>
     setNames("land")
 
+  land_r[land_r == 0] <- NA
+
   # format dimensions
   st_dimensions(land_r) <- st_dimensions(ref_grid)[1:2]
 
@@ -46,57 +48,55 @@ land_mask <- function(ref_grid) {
 
 # *****
 
-load_data <- function(
-  dir_origin_cloud, # bucket dir where all vars are located
-  dir_dest_local,
-  date_vector
-) {
-  # Downloads data corresponding to date_vector
-  # from dir_origin_cloud into dir_dest_local. Then
-  # loads it into memory
+# load_data <- function(
+#   dir_origin_cloud, # bucket dir where all vars are located
+#   dir_dest_local,
+#   date_vector
+# ) {
+#   # Downloads data corresponding to date_vector
+#   # from dir_origin_cloud into dir_dest_local. Then
+#   # loads it into memory
 
-  if (!fs::dir_exists(dir_dest_local)) {
-    fs::dir_create(dir_dest_local)
-  }
+#   if (!fs::dir_exists(dir_dest_local)) {
+#     fs::dir_create(dir_dest_local)
+#   }
 
-  # download data
+#   # download data
 
-  ff <-
-    rt_gs_list_files(dir_origin_cloud) |>
-    str_subset(".nc") |>
-    str_subset(
-      str_flatten(
-        seq(year(first(date_vector)), year(last(date_vector))),
-        "|"
-      )
-    )
+#   ff <-
+#     rt_gs_list_files(dir_origin_cloud) |>
+#     str_subset(".nc") |>
+#     str_subset(
+#       str_flatten(
+#         seq(year(first(date_vector)), year(last(date_vector))),
+#         "|"
+#       )
+#     )
 
-  ff <-
-    rt_gs_download_files(ff, dir_dest_local)
+#   ff <-
+#     rt_gs_download_files(ff, dir_dest_local)
 
-  # load data
+#   # load data
+#   ss <-
+#     ff |>
+#     map(\(f) {
+#       f |>
+#         read_mdim() |>
+#         adrop()
+#     })
 
-  ss <-
-    ff |>
-    map(\(f) {
-      f |>
-        read_mdim() |>
-        adrop()
-    })
+#   # concatenate
 
-  # concatenate
+#   ss <-
+#     do.call(c, c(ss, along = "time")) |>
+#     st_set_dimensions("time", values = date_vector)
 
-  ss <-
-    do.call(c, c(ss, along = "time")) |>
-    st_set_dimensions("time", values = date_vector)
+#   # clean up
 
-  # clean up
+#   fs::file_delete(ff)
 
-  fs::dir_delete(dir_dest_local)
-
-  return(ss)
-}
-
+#   return(ss)
+# }
 
 # *****
 
@@ -107,14 +107,14 @@ distr_params_apply <- function(x, param_names, distribution) {
   if (any(is.na(x))) {
     params <-
       rep(NA, length(param_names)) |>
-      purrr::set_names(param_names)
+      set_names(param_names)
   } else if (mean(x == 0) > 0.9) {
     params <-
-      rep(-9999, length(param_names)) |>
-      purrr::set_names(param_names)
+      rep(-99999, length(param_names)) |>
+      set_names(param_names)
   } else {
     params <-
-      distribution(lmom::samlmu(x))
+      distribution(samlmu(x))
   }
 
   return(params)
@@ -123,83 +123,77 @@ distr_params_apply <- function(x, param_names, distribution) {
 
 # *****
 
-distr_params <- function(
-  s,
-  dir_tmp_local,
-  dir_output_cloud,
-  distribution,
-  f_name_root,
-  process
-) {
-  # Fits a distribution, outputs its parameters
+# distr_params <- function(
+#   s,
+#   dir_tmp_local,
+#   dir_output_cloud,
+#   distribution,
+#   f_name_root,
+#   process
+# ) {
+#   # Fits a distribution, outputs its parameters
 
-  cl <- mirai::make_cluster(parallel::detectCores() - 1)
+#   fs::dir_create(dir_tmp_local)
 
-  fs::dir_create(dir_tmp_local)
+#   # get names from sim l-moments vector
+#   param_names <-
+#     names(distribution(c(1, 0.1, 0.1, 0.1)))
 
-  param_names <-
-    sample(10, 10, replace = T) |>
-    samlmu() |>
-    distribution() |>
-    names()
+#   walk(seq(12), \(mon) {
+#     print(str_glue("fitting month {mon} / 12"))
 
-  walk(seq(12), \(mon) {
-    print(str_glue("fitting month {mon} / 12"))
+#     if (process == "era") {
+#       #
+#       r <-
+#         s |>
+#         filter(month(time) == mon) |>
+#         units::drop_units() |>
 
-    if (process == "era") {
-      #
-      r <-
-        s |>
-        filter(month(time) == mon) |>
-        units::drop_units() |>
+#         st_apply(
+#           c(1, 2),
+#           distr_params_apply,
+#           param_names = param_names,
+#           distribution = distribution,
+#           FUTURE = T,
+#           .fname = "params"
+#         ) |>
+#         split("params")
+#       #
+#     } else if (process == "nmme") {
+#       #
+#       r <-
+#         s |>
+#         filter(month(time) == mon) |>
+#         units::drop_units() |>
 
-        st_apply(
-          c(1, 2),
-          distr_params_apply,
-          param_names = param_names,
-          distribution = distribution,
-          CLUSTER = cl,
-          .fname = "params"
-        ) |>
-        split("params")
-      #
-    } else if (process == "nmme") {
-      #
-      r <-
-        s |>
-        filter(month(time) == mon) |>
-        units::drop_units() |>
+#         st_apply(
+#           c(1, 2, 3), # pool all members to fit distr
+#           distr_params_apply,
+#           param_names = param_names,
+#           distribution = distribution,
+#           FUTURE = T,
+#           .fname = "params"
+#         ) |>
+#         split("params")
+#       #
+#     }
 
-        st_apply(
-          c(1, 2, 3), # pool all members to fit distr
-          distr_params_apply,
-          param_names = param_names,
-          distribution = distribution,
-          CLUSTER = cl,
-          .fname = "params"
-        ) |>
-        split("params")
-      #
-    }
+#     # save results
 
-    # save results
+#     f <-
+#       str_glue(
+#         "{dir_tmp_local}/{f_name_root}_{str_pad(mon, 2, 'left', '0')}.nc"
+#       )
 
-    f <-
-      str_glue(
-        "{dir_tmp_local}/{f_name_root}_{str_pad(mon, 2, 'left', '0')}.nc"
-      )
+#     rt_write_nc(r, f)
 
-    rt_write_nc(r, f)
+#     # move to cloud
+#     str_glue("gcloud storage mv {f} {dir_output_cloud}") |>
+#       system(ignore.stdout = T, ignore.stderr = T)
+#   })
 
-    # move to cloud
-    str_glue("gcloud storage mv {f} {dir_output_cloud}") |>
-      system(ignore.stdout = T, ignore.stderr = T)
-  })
-
-  fs::dir_delete(dir_tmp_local)
-  mirai::stop_cluster(cl)
-}
-
+#   fs::dir_delete(dir_tmp_local)
+# }
 
 rollsum <- function(s, k, suffix_var) {
   # Calculates a rolling sum of k width over s (stars obj);
@@ -213,7 +207,7 @@ rollsum <- function(s, k, suffix_var) {
     st_apply(
       c(1, 2),
       \(x) {
-        slider::slide_dbl(x, .f = sum, .before = k - 1, .complete = T)
+        slider::slide_sum(x, before = k - 1, complete = T)
       },
       .fname = "time"
     ) |>
